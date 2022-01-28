b962bb00d32eab448d5f3232df531402d76a9a99
anjenn <93240896+anjenn@users.noreply.github.com>
2022-01-29 01:09:47 +0900

    challenge

---
+++ 01/script.js
@@ -0,0 +1,19 @@
+let js = "amazing";
+// if (js === "amazing") alert("Javascript is Fun!");
+
+// when we want to access the console
+// console.log(40 + 8 + 23 - 10);
+
+// test
+
+const markM = 78;
+const markH = 1.69;
+
+const johnM = 92;
+const johnH = 1.95;
+
+const markBMI = markM / markH ** 2;
+const johnBMI = johnM / johnH ** 2;
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);

b9876b6c65fa9a991b1e8434d17ca7ed3b3febe2
anjenn <93240896+anjenn@users.noreply.github.com>
2022-02-22 23:01:35 +0900

    .

--- Section 09 - Data structures/challenges.js
+++ Section 09 - Data structures/challenges.js
@@ -56,6 +56,22 @@ and one array ('fieldPlayers') with all the remaining 10 field players
 const [players1, players2] = game.players;
 const [gk, ...fieldPlayers] = players1;
 
+/*2. The first player in any player array is the goalkeeper and the others are field players.
+For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name,
+and one array ('fieldPlayers') with all the remaining 10 field players */
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
 console.log(gk);
 console.log(fieldPlayers);
 //2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players

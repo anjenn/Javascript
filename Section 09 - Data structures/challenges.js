79b71cf583faf23ec6262d4da8fc037815fa2d54
anjenn <93240896+anjenn@users.noreply.github.com>
2022-02-23 23:40:42 +0900

    .

--- Section 09 - Data structures/challenges.js
+++ Section 09 - Data structures/challenges.js
@@ -54,7 +54,7 @@ and one array ('fieldPlayers') with all the remaining 10 field players
 
 //1. Create one player array for each team (variables 'players1' and 'players2')
 const [players1, players2] = game.players;
-const [gk, ...fieldPlayers] = players1;
+//const [gk, ...fieldPlayers] = players1;
 
 /*2. The first player in any player array is the goalkeeper and the others are field players.
 For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name,
@@ -91,3 +91,31 @@ console.log(gk);
 console.log(fieldPlayers);
 //2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 players1.push();
+
+//02
+/*
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+// 1.
+for (const [key, value] of game.scored.entries()) {
+  //console.log(`....`);
+  console.log(`Goal ${key}: ${value}`);
+}
+// 2.
+const oddsentries = Object.entries(game.odds);
+console.log(oddsentries);
--- Section 09 - Data structures/loopingObj.js
+++ Section 09 - Data structures/loopingObj.js
@@ -18,3 +18,11 @@ console.log(values);
 const entries = Object.entries(openingHours);
 // we don't call entries() directly on an object as we did for array
 console.log(entries); // objects transferred to arrays
+
+for (const x of entries) {
+  //console.log(`On ${key}`);
+}
+//since we know the second value in the array is an object, we can immediately destructure it
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}

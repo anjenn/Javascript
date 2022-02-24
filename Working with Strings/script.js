5923c0b79b24bedebd2cacefbce8e01c0f23706c
anjenn <93240896+anjenn@users.noreply.github.com>
2022-02-25 03:26:42 +0900

    .

--- Working with Strings/script.js
+++ Working with Strings/script.js
@@ -72,5 +72,81 @@ console.log(email === normalizedEmail);
 
 // Replacing
 const priceGB = "288,97€";
-const priceUS = priceGB.replace("€", "$");
+const priceUS = priceGB.replace("€", "$").replace(",", ".");
 console.log(priceUS);
+
+const announcement =
+  "All passengers come to boarding door 23. Boarding door 23!";
+
+console.log(announcement.replace("door", "gate"));
+console.log(announcement.replaceAll("door", "gate"));
+console.log(announcement.replace(/door/g, "gate"));
+
+// Booleans
+const plane2 = "Airbus A230neo";
+console.log(plane2.includes("Boeing"));
+console.log(plane2.startsWith("A"));
+console.log(plane2.endsWith("neo"));
+
+if (plane2.startsWith("Airbus") && plane2.endsWith("neo")) {
+  console.log("Part of the NEW Airbus family");
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase(); // essential
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are NOt allowed on board");
+  } else {
+    console.log("Welcome aboard!");
+  }
+};
+checkBaggage("I have a laptop, some food and a pocket knife");
+
+console.log("a+very+nice+string".split("+")); // splits a string into an array
+
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    //namesUpper.push(n.replace(n[0].n[0].toUpperCase()));
+  }
+  console.log(`capitalised name: ${namesUpper.join(" ")}`);
+};
+
+capitalizeName("jessica ann smity davis");
+const passenger = "jessica ann smity davis";
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+"));
+console.log(message.padStart(25, "+").padEnd(30, "+"));
+console.log("Jonas".padStart(25, "+"));
+// they make a string length of indicated number, empty space filled with a character
+
+const maskCreditCard = function (number) {
+  //converting a number to string
+  //1. String()
+  //2. concatenate with a string
+  const str = number + ""; //when one operand is a string
+  const lastFour = str.slice(-4);
+  return lastFour.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(123493280482395));
+
+// Repeat
+const message2 = "Bad weather... All Departures Delayed...";
+console.log(message2.repeat(5));
+
+// in line
+const planesInline = function (n) {
+  console.log(`There are ${n} planes in line ${"*".repeat(n)}`);
+};
+planesInline(5);

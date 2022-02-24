fa83f00aa1da374e03a8636b960bca0af477ff35
anjenn <93240896+anjenn@users.noreply.github.com>
2022-02-25 01:53:39 +0900

    .

--- Working with Strings/script.js
+++ Working with Strings/script.js
@@ -15,3 +15,25 @@ console.log(airline.lastIndexOf("r"));
 console.log(airline.indexOf("portugal"));
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === "B" || s === "E") console.log(`you've got the middle seat`);
+  else console.log("You are lucky");
+};
+
+checkMiddleSeat("118");
+checkMiddleSeat("23C");
+checkMiddleSeat("3E");
+
+// why methods work on strings?
+// when we call a string js bts basically converts it into an object
+
+console.log(typeof new String("jonas"));

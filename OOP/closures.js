df09572155e4a23c278ec39682da844719ce76de
anjenn <93240896+anjenn@users.noreply.github.com>
2022-03-10 12:15:51 +0100

    .

--- OOP/closures.js
+++ OOP/closures.js
@@ -11,3 +11,26 @@ const secureBooking = function () {
 };
 
 const booker = secureBooking();
+// this booker is a variable that exists in the global environment
+
+booker();
+booker();
+booker();
+
+// we can see that the passengerCount increments
+
+// !! closures make the functins remember all the variables the function used in the history
+
+// secure Booking leaves the call stack after its execution
+// and then we run booker function in the global scope
+// new empty execution context for booker() is then created on top of the call stack
+// in scope chain, booker() scope then becomes the child of the global scope, just like secureBooking() scope
+
+// the rempty booker function needs access to passsenger count!
+
+/* ANY function always has access to the variable environment of the execution context
+where the function was created*/
+// closure: Variable environment attached to the function, exactly as it was at the time and place the function was created
+
+// booker function was born in the execution context of secureBooking
+// therefore booker function has access to the passengerCount
